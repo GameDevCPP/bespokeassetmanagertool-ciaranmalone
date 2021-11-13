088a35e49e48f863e6b623a5f420a4d77a3b4f6b
ciaranmalone <ciaranmalone27@gmail.com>
2021-11-13 23:42:30 +0000

    button input

--- main.cpp
+++ main.cpp
@@ -1,131 +1,129 @@
-#include "json.hpp"
 #include <SFML/Graphics.hpp>
-#include "TileMap.cpp"
 #include <iostream>
-#include <vector>
+#include "TileMap.cpp"
+#include "json.hpp"
 #include <fstream>
 
-
 namespace ns{
-struct MapForJsonStruct{
-    std::vector<int> map;
-    std::string textureForMap;
-    std::vector<int> tileSize, mapSize; 
 
-    const int* get_level()
-    {
-        //since vector must store data contiguously, the following works for array
-        int* a = &map[0];
-        return a;
-    }
-};
+    struct MapForJsonStruct{
+        std::vector<int> map;
+        std::string textureForMap;
+        std::vector<int> tileSize, mapSize; 
 
-void to_json(nlohmann::json& j, const MapForJsonStruct& ms){
-    j = nlohmann::json{
-        {"map",ms.map},
-        {"tilesize",ms.tileSize},
-        {"mapsize",ms.mapSize},
-        {"maptexture",ms.textureForMap}
+        const int* get_level()
+        {
+            //since vector must store data contiguously, the following works for array
+            int* a = &map[0];
+            return a;
+        }
     };
+
+    void to_json(nlohmann::json& j, const MapForJsonStruct& ms){
+        j = nlohmann::json{
+            {"map",ms.map},
+            {"tilesize",ms.tileSize},
+            {"mapsize",ms.mapSize},
+            {"maptexture",ms.textureForMap}
+        };
+    }
+
+    void from_json(const nlohmann::json& j, MapForJsonStruct& ms){
+        j.at("map").get_to(ms.map);
+        j.at("tilesize").get_to(ms.tileSize);
+        j.at("mapsize").get_to(ms.mapSize);
+        j.at("maptexture").get_to(ms.textureForMap);
+    }
 }
-void from_json(const nlohmann::json& j, MapForJsonStruct& ms){
-    j.at("map").get_to(ms.map);
-    j.at("tilesize").get_to(ms.tileSize);
-    j.at("mapsize").get_to(ms.mapSize);
-    j.at("maptexture").get_to(ms.textureForMap);
+
+void updateJson(ns::MapForJsonStruct ms) {
+
+    using nlohmann::json;
+
+    json j2 = ms;
+
+    std::ofstream outJson("map.json");
+    outJson << j2 << std::endl;
+
 }
-
-
-}
-
-
-void DrawMap(ns::MapForJsonStruct ms);
 
 int main()
 {
+    int TileType = 1;
+
     using nlohmann::json;
-    std::ofstream outJson("map.json");
-  
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
-    window.setVerticalSyncEnabled(true);
-    
-    sf::Vector2u widthAndHeight(16, 8), tileSize(32,32);
-    
-    
-    //output map as json object
-    json jsonMap={
-        {"map",
-            {0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-            0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-            1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
-            0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
-            0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0 , 0,
-            0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
-            2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
-            0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1}},
-        {"tilesize",{tileSize.x,tileSize.y}},
-        {"mapsize",{widthAndHeight.x,widthAndHeight.y}},
-        {"maptexture","tileset.png"}
-    };
-    
-    
-    ns::MapForJsonStruct map2;
-    json j = map2;
-    
-    
-    //write json object:
-    outJson << jsonMap<< std::endl;
-    
-    
-    std::ifstream i("map.json");
-    i >> j;
-    std::cout << j;
-    auto p2 = j.get<ns::MapForJsonStruct>();
-    std::cout <<std::endl<<" p2 =";
-    for (auto i : p2.map)
-        std::cout << i << ' ';
-    std::cout <<  ", mapsize = [" << p2.mapSize[0] << ","<< p2.mapSize[1]<<"]"<<std::endl;
-    // create the tilemap from the level definition
-    TileMap map;
-   
-    //read in our data-driven-game -- well just a map - but it's a start!
-    DrawMap(p2);
 
-    return EXIT_SUCCESS;
-   
-};
+    sf::RenderWindow window(sf::VideoMode(800, 600), "TileMap Maker V0.1", sf::Style::Close |  sf::Style::Resize);
 
-void DrawMap(ns::MapForJsonStruct ms)
-{
+    std::ifstream file ("map.json");
 
-    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
+    json j;
+    file >> j;
+    auto ms = j.get<ns::MapForJsonStruct>();
+
     window.setVerticalSyncEnabled(true);
 
-    // define the level with an array of tile indices
-    auto level = ms.get_level();//I know return type is const int*.
-
-    // create the tilemap from the level definition
     TileMap map;
-    
-    if (map.load(ms.textureForMap, sf::Vector2u(ms.tileSize[0], ms.tileSize[1]), ms.get_level(), ms.mapSize[0], ms.mapSize[1])) 
+
+    map.load(ms.textureForMap, sf::Vector2u(ms.tileSize[0], ms.tileSize[1]), ms.get_level(), ms.mapSize[0], ms.mapSize[1]);
+
+    while (window.isOpen())
     {
-
-        // run the main loop
-        while (window.isOpen())
+        //window pull event
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            // handle events
-            sf::Event event;
-            while (window.pollEvent(event))
+            switch (event.type)
             {
-                if(event.type == sf::Event::Closed)
-                    window.close();
-            }
 
-            // draw the map
-            window.clear();
-            window.draw(map);
-            window.display();
+            case sf::Event::Closed:
+                updateJson(ms);
+                window.close();
+                break;
+            
+            case sf::Event::Resized:
+                // std::cout << " Window Width: " << event.size.width << "Window Height" << event.size.height << std::endl;
+                printf("Window Width: %i Window Height %i \n", event.size.width, event.size.height);
+                break;
+
+            case sf::Event::TextEntered:
+                    printf("%c", event.text.unicode);
+                break;
+
+            default:
+                break;
+            }
         }
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+            int col = floor(mousePos.x/ms.tileSize[0] ) + 1;          
+            int row = floor(mousePos.y/ms.tileSize[1] ) + 1; 
+
+            if (col <= ms.mapSize[0] && row <= ms.mapSize[1]) {
+                
+                ms.map[((row-1) * ms.mapSize[0]) + (col-1)] = TileType;
+                map.load(ms.textureForMap, sf::Vector2u(ms.tileSize[0], ms.tileSize[1]), ms.get_level(), ms.mapSize[0], ms.mapSize[1]);
+                printf("x: %i y %i \n", col, row);
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+            TileType = 0;
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+            TileType = 1;
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
+            TileType = 2;
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4))
+            TileType = 3;
+        
+
+        window.clear();
+        window.draw(map);
+        window.display();
     }
+
+    return 0;
 }
+
